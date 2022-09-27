a0afbde753218bdc2fcd9a11fc087f0db797e636
thudari <thudariabib@gmail.com>
2022-09-27 17:17:21 -0300

    Main view controller

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module workshopJavafxJdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
-	
 	opens application to javafx.graphics, javafx.fxml;
+	exports gui to javafx.fxml;
+	opens gui to javafx.fxml;
 }
