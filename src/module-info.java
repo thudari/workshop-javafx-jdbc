ef1ee3a6adcf374c390af4c0043f2d3e382b0ce6
thudari <thudariabib@gmail.com>
2022-09-28 11:40:42 -0300

    DepartmentService

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,8 @@
 module workshopJavafxJdbc {
-	requires javafx.controls;
-	requires javafx.fxml;
-	opens application to javafx.graphics, javafx.fxml;
-	exports gui to javafx.fxml;
-	opens gui to javafx.fxml;
+	   requires javafx.controls;
+       requires javafx.fxml;
+       opens application to javafx.graphics, javafx.fxml;
+       opens gui to javafx.graphics, javafx.fxml;
+       opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
+       opens model.services to javafx.graphics, javafx.fxml;
 }
