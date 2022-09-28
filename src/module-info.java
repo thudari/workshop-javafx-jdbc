d455be76f2b3a4a707b5296dc9010f3abec326af
thudari <thudariabib@gmail.com>
2022-09-28 17:59:26 -0300

    DepartmentForm (dialog) design

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module workshopJavafxJdbc {
 	   requires javafx.controls;
        requires javafx.fxml;
 	requires java.sql;
+	requires javafx.graphics;
        opens application to javafx.graphics, javafx.fxml;
        opens gui to javafx.graphics, javafx.fxml;
        opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
